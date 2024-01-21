0dd87fbab6aa69ab121464f24c0dca06a611c35d
Abdullah AL Hinaey <101839293+AbdullahALX@users.noreply.github.com>
2024-01-20 19:16:36 -0500

    prod2

--- backend/index.js
+++ backend/index.js
@@ -49,12 +49,12 @@ mongoose
 //   );
 // });
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('frontend/dist'));
-  app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
-  );
-}
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static('frontend/dist'));
+//   app.get('*', (req, res) =>
+//     res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
+//   );
+// }
 
 const PORT = process.env.PORT || 3001;
 
